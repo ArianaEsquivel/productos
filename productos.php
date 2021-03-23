be867b2a61bfbfc7403c536077f43348bf3ad067
ErianaEsquivel <19170089@uttcampus.edu.mx>
2021-03-23 16:22:46 -0600

    A ver si ya

--- productos.php
+++ productos.php
@@ -1,57 +1,21 @@
 <?php
-private $listP = [];
-$listP = [
-    {
-        $id = 1;
-        $categoria = "Navideños";
-        $nombre = "Obeja para sendero";
-        $precio = 300;
-    },
-    {
-        $id = 2;
-        $categoria = "Muebles";
-        $nombre = "Ropero";
-        $precio = 500;
-    },
-    {
-        $id = 3;
-        $categoria = "Escolar";
-        $nombre = "Lapicero";
-        $precio = 80;
-    },
-    {
-        $id = 4;
-        $categoria = "Navideños";
-        $nombre = "Sombrero";
-        $precio = 220;
-    },
-    {
-        $id = 5;
-        $categoria = "Navideños";
-        $nombre = "Árbol decorativo";
-        $precio = 600;
-    },
-    {
-        $id = 6;
-        $categoria = "Navideños";
-        $nombre = "Tablero de uno";
-        $precio = 210;
-    },
-]
+$listJ = file_get_contents("lista.json");
+$lista = json_decode($listJ, true);
 
-    foreach ($listP as $obj) 
+foreach ($lista as $obj) 
+{
+    if($obj["categoria"] == "Navideños" && strpos($obj["nombre"], "ero") !== false)
     {
-        $resultado = $fila->cantidad;
-        $tel = $fila->telefono;
-        $co = $fila->correo;
-        if($obj->categoria == "Navideños" && strpos($obj->nombre, "ero") === true && $obj->precio > 200 &&  $obj->precio < 400 )
-        {
-            echo $obj->id;
-            echo $obj->nombre;
-            echo $obj->categoria;
-            echo $obj->precio;
-        }
+        echo "<li>".$obj['id']."</li>";
+        echo "\n";
+        echo "<li>".$obj['nombre']."</li>";
+        echo "\n";
+        echo "<li>".$obj['categoria']."</li>";
+        echo "\n";
+        echo "<li>".$obj['precio']."</li>";
+        echo "\n";
     }
     
-
+}
+    
 ?>
--- productosh.php
+++ productosh.php
@@ -5,8 +5,11 @@
     <title>Cherry Spa</title>
   </head>
   <body>
-<?php 
-include 'productos.php';
-?>
+      <h4>Lista de productos de categoría navideños con cadena "ero" entre $200 y $400</h4>
+      <ul>
+        <?php 
+        include 'productos.php';
+        ?>
+      </ul>    
   </body>
 </html>
