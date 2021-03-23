34e597657729ef39386e0a0c2e5336e424adc69b
ErianaEsquivel <19170089@uttcampus.edu.mx>
2021-03-23 16:35:39 -0600

    Detalles2

--- productos.php
+++ productos.php
@@ -6,13 +6,13 @@ foreach ($lista as $obj)
 {
     if($obj["categoria"] == "Navideños" && strpos($obj["nombre"], "ero") !== false)
     {
-        echo "<li>ID producto".$obj['id']."</li>"
-            echo "<ul>"
+        echo "<li>ID producto".$obj['id']."</li>";
+            echo "<ul>";
             echo "<li>id: ".$obj['id']."</li>";
             echo "<li>Categoría: ".$obj['categoria']."</li>";
             echo "<li>Nombre: ".$obj['nombre']."</li>";
             echo "<li>Precio: $".$obj['precio']."</li>";
-            echo "</ul>"
+            echo "</ul>";
     }
     
 }
